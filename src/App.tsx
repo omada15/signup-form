b1f08b8afc441a316cae8b111a84a08cd5d110de
Daniel <dan.senchukov@gmail.com>
2026-02-12 19:02:02 -0500

    front

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
-import { useState } from 'react'
+import { useState } from "react"
+import DropdownSearch from "./Components/DropdownSearch"
 
 function App() {
-    const [count, setCount] = useState(0)
-    const [id, setid] = useState("")
+
+    const [goon, setgoon] = useState<string>("");
 
     return (
-        /* 'w-full min-h-screen' ensures the background actually fills the page */
         <div className="bg-gray-800 min-h-screen w-full flex flex-col items-center justify-center text-white">
-            <input
-                type="text"
-                className="bg-gray-200 text-black p-2 rounded"
-                onChange={(e) => setid(e.target.value)}
+            {goon}
+            <DropdownSearch
+                label="Search name"
+                options={["SAM GOON", "SAM BUST", "21079", "WILL DING", "MASON DONG", "DANIEL SENCHUKOV"]}
+                placeholder="Search..."
+                onChange={setgoon}
             />
-            <h1 className="text-4xl mt-4">Vite + React</h1>
-
-            <button
-                className="mt-4 px-6 py-2 bg-red-600 text-white font-bold rounded hover:bg-red-700 transition"
-                onClick={() => setCount((count) => count + 1)}>
-                count is {count}
-            </button>
-
-            <p className="mt-8 text-gray-400">
-                Current ID: {id}
-            </p>
         </div>
     )
 }
